49f9935df663a6577a548c65bdaff08109989ae2
Yicheng Wang PC <wang_930109@qq.com>
2020-11-11 05:49:45 +0800

    betas=(0.9, 0.999) in adammw

--- utils/optimizers.py
+++ utils/optimizers.py
@@ -3,21 +3,21 @@ from torch.optim.adamw import AdamW
 
 
 class AdamMW(AdamW):
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.9), eps=1e-8,
-                 weight_decay=1e-2, amsgrad=False):
-        if not 0.0 <= lr:
-            raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0.0 <= eps:
-            raise ValueError("Invalid epsilon value: {}".format(eps))
-        if not 0.0 <= betas[0] < 1.0:
-            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
-        if not 0.0 <= betas[1] < 1.0:
-            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
-        if not 0.0 <= weight_decay:
-            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
-        defaults = dict(lr=lr, betas=betas, eps=eps,
-                        weight_decay=weight_decay, amsgrad=amsgrad)
-        super(AdamW, self).__init__(params, defaults)
+    # def __init__(self, params, lr=1e-3, betas=(0.9, 0.9), eps=1e-8,
+    #              weight_decay=1e-2, amsgrad=False):
+    #     if not 0.0 <= lr:
+    #         raise ValueError("Invalid learning rate: {}".format(lr))
+    #     if not 0.0 <= eps:
+    #         raise ValueError("Invalid epsilon value: {}".format(eps))
+    #     if not 0.0 <= betas[0] < 1.0:
+    #         raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+    #     if not 0.0 <= betas[1] < 1.0:
+    #         raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+    #     if not 0.0 <= weight_decay:
+    #         raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+    #     defaults = dict(lr=lr, betas=betas, eps=eps,
+    #                     weight_decay=weight_decay, amsgrad=amsgrad)
+    #     super(AdamW, self).__init__(params, defaults)
 
     @torch.no_grad()
     def step(self, closure=None):
