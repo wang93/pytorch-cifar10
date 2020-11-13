d29cb55d4bfbe3d7be85ad6f7e881df47231e8bc
YIcheng-HP <534579263@qq.com>
2020-11-13 09:42:28 +0800

    no clip in srl

--- SampleRateLearning/sampler.py
+++ SampleRateLearning/sampler.py
@@ -75,7 +75,7 @@ class SampleRateBatchSampler(SampleRateSampler):
 
         # b_num = binomial(self.batch_size, self.pos_rate)
         b_num = round(self.batch_size * self.pos_rate)
-        b_num = int(clip(b_num, 1, self.batch_size-1))
+        # b_num = int(clip(b_num, 1, self.batch_size-1))
         a_num = self.batch_size - b_num
         batch = self.sample_agents[0].select(a_num) + self.sample_agents[1].select(b_num)
 
