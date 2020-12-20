d5431dc238e2af37138cf558544ce91bcea93968
YIcheng-HP <534579263@qq.com>
2020-12-20 14:49:40 +0800

    fabipc

--- SampleRateLearning/loss.py
+++ SampleRateLearning/loss.py
@@ -125,7 +125,9 @@ class SRL_BCELoss(nn.Module):
         if self.norm:
             loss = (train_neg_loss + train_pos_loss) / 2.
         else:
-            pos_rate = self.pos_rate.detach()
+            pos_rate = self.pos_rate
+            if isinstance(pos_rate, torch.Tensor):
+                pos_rate = pos_rate.detach()
             losses = torch.cat((train_neg_losses * (1. - pos_rate), train_pos_losses * pos_rate), dim=0)
             loss = losses.mean() * 2.
 
