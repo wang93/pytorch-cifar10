201c50163ac26f6bc1c8828d7ded34b47e3fc8e4
Yicheng-gov <wang_930109@qq.com>
2020-12-23 16:19:33 +0800

    1. modify sbn0

--- SampleRateLearning/special_batchnorm/batchnorm0.py
+++ SampleRateLearning/special_batchnorm/batchnorm0.py
@@ -67,14 +67,18 @@ class _BatchNorm(origin_BN):
             # y = (input - self.expand(di_mean, sz)) \
             #     / self.expand(torch.sqrt(di_var + self.eps), sz)
 
+            # y = (input - di_mean.view(new_size)) \
+            #     / torch.full_like(di_var, 1e-2).max(di_var).view(new_size)
+
             y = (input - di_mean.view(new_size)) \
-                / torch.full_like(di_var, 1e-2).max(di_var).view(new_size)
+                / torch.full_like(di_var, 1e-2).max(di_var.sqrt()).view(new_size)
 
         else:
             # y = (input - self.expand(self.running_mean, sz)) \
             #     / self.expand(torch.sqrt(self.running_var + self.eps), sz)
+
             y = (input - self.running_mean.view(new_size)) \
-                / torch.full_like(self.running_var, 1e-2).max(self.running_var).view(new_size)
+                / torch.full_like(self.running_var, 1e-2).max(self.running_var.sqrt()).view(new_size)
 
         if self.affine:
             z = y * self.weight.view(new_size) + self.bias.view(new_size)
